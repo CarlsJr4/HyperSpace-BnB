380e6b9c84ce2e703a0b01449da18347bed77263
Carl Dungca <carl.dungca@yahoo.com>
2023-01-06 13:33:15 -0800

    Fixed gradient and planet mini backgrounds

--- components/locations/LocationCard.js
+++ components/locations/LocationCard.js
@@ -2,6 +2,8 @@ import Image from 'next/image';
 import LinkButton from '../core/LinkButton';
 import { GiRingedPlanet } from 'react-icons/gi';
 
+// TODO: Pagination
+
 export default function LocationCard({
   key,
   title,
@@ -12,19 +14,27 @@ export default function LocationCard({
   desc,
 }) {
   return (
-    <div key={key} className="bg-gray-800 p-4 rounded-md">
-      <div className="grid grid-cols-listings gap-8">
+    <div key={key} className="bg-[#25192e] p-4 rounded-md">
+      <div className="grid grid-cols-1 lg:grid-cols-listings gap-8">
         <Image
           src={src}
           alt="This is a randomly generated image"
           width="300"
           height="300"
-          className="rounded-md"
+          className="rounded-md justify-self-center lg:justify-self:start"
         />
         <div className="flex flex-col gap-2">
           <p className="boldText text-2xl">{title}</p>
           <p className="boldText">{location}</p>
-          <p className="flex items-center gap-1">
+          <p
+            className={`
+							${body === 'Mars' ? 'bg-red-500' : ''}
+							${body === 'Luna' ? 'bg-white' : ''}
+							${body === 'Jupiter' ? 'bg-blue-500' : ''}
+							${body === 'Neptune' ? 'bg-green-500' : ''}
+              flex items-center gap-1 bg-opacity-20 px-5 py-2 rounded-lg max-w-max
+            `}
+          >
             <GiRingedPlanet size="1.5rem" />
             <span>{body}</span>
           </p>
