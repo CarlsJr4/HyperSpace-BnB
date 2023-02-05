ab092abeb71ffd0217870155e16b5a17d06a3268
Carl Dungca <carl.dungca@yahoo.com>
2023-02-04 21:50:22 -0800

    Responsive filter buttons

--- components/locations/FilterButton.js
+++ components/locations/FilterButton.js
@@ -19,7 +19,7 @@ export default function FilterButton({
       aria-pressed={isActive}
       onClick={() => handleClick()}
       className={`${isActive ? 'border-opacity-100' : 'border-opacity-20'}
-			border-fuchsia-500 border-2 rounded-md px-6 py-2 hover:scale-110 transition-all flex items-center justify-center gap-2`}
+			border-fuchsia-500 border-2 rounded-md px-6 py-2 hover:scale-110 transition-all flex items-center justify-start sm:justify-center gap-2`}
     >
       {isActive ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
       {children}
